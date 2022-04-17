197354d814b76fb9a2d4b1fb56499a68ad203a99
Mateusz Kubisiak <matesz1kb@gmail.com>
2022-04-18 01:54:58 +0200

    Created memories controllers

---
+++ server/controllers/memories.js
@@ -0,0 +1,51 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_MEMORIES = [
+   {
+      id: "m1",
+      title: "My dog",
+      description: "Bla bla bla",
+      location: {
+         lat: 40.7484474,
+         lng: -73.9871516,
+      },
+      creator: "u1",
+   },
+];
+
+const getMemoryById = (req, res, next) => {
+   const memoryId = req.params.mid;
+   const memory = DUMMY_MEMORIES.find((m) => {
+      return m.id === memoryId;
+   });
+
+   if (!memory) {
+      throw (error = new HttpError(
+         "Could not find Memory for provided id",
+         404
+      ));
+   }
+
+   res.json({ memory });
+};
+
+const getMemoryByUserId = (req, res, next) => {
+   const userId = req.params.uid;
+   const memory = DUMMY_MEMORIES.find((m) => {
+      return m.creator === userId;
+   });
+
+   if (!memory) {
+      return next(
+         new HttpError("Could not find Memory for provided user id", 404)
+      );
+   }
+
+   res.json({ memory });
+};
+
+const createMemory = (req, res, next) => {};
+
+exports.getMemoryById = getMemoryById;
+exports.getMemoryByUserId = getMemoryByUserId;
+exports.createMemory = createMemory;
