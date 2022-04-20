38e8228f55831915204d13d3a6a12570abfade76
Mateusz Kubisiak <matesz1kb@gmail.com>
2022-04-20 15:26:56 +0200

    Shared styles

---
+++ client/src/styles/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+   color: {
+      orange: "#fab84f",
+   },
+   shadow: {
+      normal: "8px 8px 12px -12px rgba(97, 97, 97, 0.5)",
+   },
+   mixins: {
+      xs: "@media (max-width: 400px)",
+      sm: "@media (max-width: 576px)",
+      md: "@media (max-width: 768px)",
+      lg: "@media (max-width: 992px)",
+      xl: "@media (max-width: 1200px)",
+      xxl: "@media (max-width: 1400px)",
+   },
+};
+
+export default theme;
