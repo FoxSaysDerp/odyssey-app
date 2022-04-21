9c080572bee15421b6fbe01c15cc91e49c4c97ac
Mateusz Kubisiak <matesz1kb@gmail.com>
2022-04-21 21:18:29 +0200

    Added Input with validation

---
+++ client/src/common/components/Input.js
@@ -0,0 +1,125 @@
+import { useReducer } from "react";
+import { validate } from "../../util/validators";
+import styled from "styled-components";
+
+const InputWrapper = styled.div`
+   display: block;
+   position: relative;
+`;
+const InputLabel = styled.label`
+   display: block;
+`;
+
+const InputField = styled.input`
+   display: block;
+   width: calc(100% - 6px);
+   padding: 3px 4px;
+   margin: 0;
+   ${({ isValid, isTouched }) =>
+      !isValid &&
+      isTouched &&
+      `
+   border: 1px solid #f5c9cd;
+   background-color: #f8d7da;
+   `}
+`;
+
+const InputTextArea = styled.textarea`
+   display: block;
+   width: 100%;
+   ${({ isValid, isTouched }) =>
+      !isValid &&
+      isTouched &&
+      `
+   border: 1px solid #f5c9cd;
+   background-color: #f8d7da;
+   `}
+`;
+
+const ErrorText = styled.p`
+   display: block;
+   font-size: 0.8rem;
+   color: #831c21;
+   background-color: #f8d7da;
+   border: 1px solid #f5c9cd;
+   border-radius: 8px;
+   padding: 8px;
+   width: calc(100% - 16px);
+   text-align: center;
+`;
+
+const inputReducer = (state, action) => {
+   switch (action.type) {
+      case "CHANGE":
+         return {
+            ...state,
+            value: action.val,
+            isValid: validate(action.val, action.validators),
+         };
+      case "TOUCH":
+         return {
+            ...state,
+            isTouched: true,
+         };
+      default:
+         return state;
+   }
+};
+
+const Input = ({
+   id,
+   label,
+   type,
+   element,
+   placeholder,
+   rows,
+   errorText,
+   validators,
+}) => {
+   const [inputState, dispatch] = useReducer(inputReducer, {
+      value: "",
+      isValid: false,
+      isTouched: false,
+   });
+
+   const changeHandler = (e) => {
+      dispatch({ type: "CHANGE", val: e.target.value, validators: validators });
+   };
+
+   const touchHandler = () => {
+      dispatch({ type: "TOUCH" });
+   };
+
+   return (
+      <InputWrapper>
+         {label && <InputLabel htmlFor={id}>{label}</InputLabel>}
+         {element === "textarea" ? (
+            <InputTextArea
+               id={id}
+               rows={rows || 3}
+               onChange={changeHandler}
+               onBlur={touchHandler}
+               value={inputState.value}
+               isValid={inputState.isValid}
+               isTouched={inputState.isTouched}
+            />
+         ) : (
+            <InputField
+               id={id}
+               type={type}
+               placeholder={placeholder || null}
+               onChange={changeHandler}
+               onBlur={touchHandler}
+               value={inputState.value}
+               isValid={inputState.isValid}
+               isTouched={inputState.isTouched}
+            />
+         )}
+         {!inputState.isValid && inputState.isTouched && errorText && (
+            <ErrorText>{errorText}</ErrorText>
+         )}
+      </InputWrapper>
+   );
+};
+
+export default Input;
--- client/src/memories/pages/NewMemory.js
+++ client/src/memories/pages/NewMemory.js
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+
+import Input from "../../common/components/Input";
+import { VALIDATOR_REQUIRE } from "../../util/validators";
+
+const MemoryFormWrapper = styled.form`
+   display: flex;
+   height: 100vh;
+   justify-content: center;
+   align-items: center;
+`;
+
 const NewMemory = () => {
-   return <div>New Memory Page</div>;
+   return (
+      <MemoryFormWrapper>
+         <Input
+            type="text"
+            placeholder="Title"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a valid title."
+         />
+      </MemoryFormWrapper>
+   );
 };
 
 export default NewMemory;
