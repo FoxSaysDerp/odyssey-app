29db958fd6023b55164406678f950cef27969212
Mateusz Kubisiak <matesz1kb@gmail.com>
2022-04-22 22:08:28 +0200

    Added New Memory functionality

--- client/src/memories/pages/NewMemory.js
+++ client/src/memories/pages/NewMemory.js
@@ -1,15 +1,29 @@
-import { useCallback, useReducer } from "react";
+import { useContext } from "react";
+import { AuthContext } from "../../common/context/auth-context";
 import styled from "styled-components";
+import { toast } from "react-toastify";
+import { useHistory } from "react-router-dom";
 
 import { useForm } from "../../common/hooks/useForm";
+import { useHttpClient } from "../../common/hooks/useHttpClient";
 import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../util/validators";
 
+import Spinner from "../../common/components/Spinner";
 import Input from "../../common/components/Input";
 import { buttonGradient } from "../../common/components/Button";
+
+const MemoryFormContainer = styled.div`
+   display: flex;
+   width: 100%;
+   height: 100vh;
+   justify-content: center;
+   align-items: center;
+`;
 
 export const MemoryFormWrapper = styled.form`
    display: flex;
    height: 100vh;
+   width: 100%;
    justify-content: center;
    align-items: center;
    flex-direction: column;
@@ -21,6 +35,8 @@ const SubmitMemoryButton = styled.button`
 `;
 
 const NewMemory = () => {
+   const auth = useContext(AuthContext);
+
    const { formState, inputHandler } = useForm(
       {
          title: {
@@ -35,33 +51,71 @@ const NewMemory = () => {
       false
    );
 
-   const memorySubmitHandler = (e) => {
+   const { isLoading, sendRequest, error } = useHttpClient();
+
+   const history = useHistory();
+
+   const memorySubmitHandler = async (e) => {
       e.preventDefault();
-      console.log(formState.inputs);
+      try {
+         await sendRequest(
+            "http://localhost:5000/api/memories",
+            "POST",
+            { "Content-Type": "application/json" },
+            JSON.stringify({
+               title: formState.inputs.title.value,
+               description: formState.inputs.description.value,
+               creator: auth.userId,
+            })
+         );
+         toast.success("Successfully created a Memory!", {
+            position: "bottom-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: false,
+            progress: 0,
+         });
+         history.push("/");
+      } catch (err) {
+         toast.error(`${error || "Something went wrong, please try again"}`, {
+            position: "bottom-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: false,
+            progress: 0,
+         });
+      }
    };
 
    return (
-      <MemoryFormWrapper onSubmit={memorySubmitHandler}>
-         <Input
-            id="title"
-            type="text"
-            placeholder="Title"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter a valid title."
-            onInput={inputHandler}
-         />
-         <Input
-            id="description"
-            element="textarea"
-            placeholder="Description"
-            validators={[VALIDATOR_MINLENGTH(5)]}
-            errorText="Please enter a valid description (at least 5 character)."
-            onInput={inputHandler}
-         />
-         <SubmitMemoryButton type="submit" disabled={!formState.isValid}>
-            Add Memory
-         </SubmitMemoryButton>
-      </MemoryFormWrapper>
+      <MemoryFormContainer>
+         <MemoryFormWrapper onSubmit={memorySubmitHandler}>
+            {isLoading && <Spinner asOverlay />}
+            <Input
+               id="title"
+               type="text"
+               placeholder="Title"
+               validators={[VALIDATOR_REQUIRE()]}
+               errorText="Please enter a valid title."
+               onInput={inputHandler}
+            />
+            <Input
+               id="description"
+               element="textarea"
+               placeholder="Description"
+               validators={[VALIDATOR_MINLENGTH(5)]}
+               errorText="Please enter a valid description (at least 5 character)."
+               onInput={inputHandler}
+            />
+            <SubmitMemoryButton type="submit" disabled={!formState.isValid}>
+               Add Memory
+            </SubmitMemoryButton>
+         </MemoryFormWrapper>
+      </MemoryFormContainer>
    );
 };
 
