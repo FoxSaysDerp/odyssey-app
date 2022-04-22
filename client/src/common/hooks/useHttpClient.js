9e36f7034bc9a4b5ab79b134e6c41138ab4294a3
Mateusz Kubisiak <matesz1kb@gmail.com>
2022-04-22 19:59:42 +0200

    Created Http hook

---
+++ client/src/common/hooks/useHttpClient.js
@@ -0,0 +1,45 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+
+export const useHttpClient = () => {
+   const [isLoading, setIsLoading] = useState(false);
+   const [error, setError] = useState();
+
+   const activeHttpRequests = useRef([]);
+
+   const sendRequest = useCallback(
+      async (url, method = "GET", body = null, headers = {}) => {
+         setIsLoading(true);
+
+         const httpAbortCtrll = new AbortController();
+         activeHttpRequests.current.push(httpAbortCtrll);
+
+         try {
+            const response = await fetch(url, {
+               method,
+               body,
+               headers,
+               signal: httpAbortCtrll,
+            });
+            const responseData = await response.json();
+
+            if (!response.ok) {
+               throw new Error(responseData.messsage);
+            }
+
+            return responseData;
+         } catch (err) {
+            setError(err);
+         }
+         setIsLoading(false);
+      },
+      []
+   );
+
+   useEffect(() => {
+      return () => {
+         activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+      };
+   }, []);
+
+   return { isLoading, error, sendRequest };
+};
