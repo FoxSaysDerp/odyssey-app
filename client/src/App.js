761dba708d298a84473ac07e18273f0c509176ac
Mateusz Kubisiak <matesz1kb@gmail.com>
2022-05-02 15:09:21 +0200

    Transformed auth to custom hook

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,14 @@ import "reset-css";
 import "./styles/app.scss";
 import "react-toastify/dist/ReactToastify.css";
 
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback } from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import { AuthContext } from "./common/context/auth-context";
 import { ThemeContext } from "./common/context/theme-context";
-import moment from "moment";
-import { toast } from "react-toastify";
+
+import { useAuth } from "./common/hooks/useAuth";
 
 import { Main } from "./styles/Main";
-
 import Header from "./common/components/Layout/Header";
 import { ToastContainer } from "react-toastify";
 
@@ -23,54 +22,8 @@ import NewMemory from "./memories/pages/NewMemory";
 import UpdateMemory from "./memories/pages/UpdateMemory";
 import UserMemories from "./memories/pages/UserMemories";
 
-let logoutTimer;
-
 const App = () => {
-   const [token, setToken] = useState(false);
-   const [tokenExpirationDate, setTokenExpirationDate] = useState();
-   const [userId, setUserId] = useState(null);
-
-   const login = useCallback((uid, token, expirationDate) => {
-      setToken(token);
-      setUserId(uid);
-
-      const tokenExpirationDate = expirationDate || moment().add(1, "h");
-      setTokenExpirationDate(tokenExpirationDate);
-
-      localStorage.setItem(
-         "userData",
-         JSON.stringify({
-            userId: uid,
-            token: token,
-            expiration: tokenExpirationDate.toISOString(),
-         })
-      );
-      toast.success("You have succesfully logged in!", {
-         position: "bottom-right",
-         autoClose: 2000,
-         hideProgressBar: false,
-         closeOnClick: true,
-         pauseOnHover: true,
-         draggable: false,
-         progress: 0,
-      });
-   }, []);
-
-   const logout = useCallback(() => {
-      setToken(null);
-      setUserId(null);
-      localStorage.removeItem("userData");
-
-      toast.info("You have logged out.", {
-         position: "bottom-right",
-         autoClose: 2000,
-         hideProgressBar: false,
-         closeOnClick: true,
-         pauseOnHover: true,
-         draggable: false,
-         progress: 0,
-      });
-   }, []);
+   const { token, login, logout, userId } = useAuth();
 
    const [isMenuExpanded, setIsMenuExpanded] = useState(true);
 
@@ -103,31 +56,6 @@ const App = () => {
          </Switch>
       );
    }
-
-   useEffect(() => {
-      if (token && tokenExpirationDate) {
-         const remainingTime = moment(tokenExpirationDate) - moment();
-
-         logoutTimer = setTimeout(logout, remainingTime);
-      } else {
-         clearTimeout(logoutTimer);
-      }
-   }, [token, logout, tokenExpirationDate]);
-
-   useEffect(() => {
-      const storedData = JSON.parse(localStorage.getItem("userData"));
-      if (
-         storedData &&
-         storedData.token &&
-         moment(storedData.expiration) > moment()
-      ) {
-         login(
-            storedData.userId,
-            storedData.token,
-            moment(storedData.expiration)
-         );
-      }
-   }, [login]);
 
    return (
       <AuthContext.Provider
